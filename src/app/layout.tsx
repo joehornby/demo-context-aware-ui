13082eebfed25bd068c7cb51727d9ed22e2b4068
Joe Hornby <joehornby@gmail.com>
2025-09-27 20:12:04 +0100

    style: update body background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${saans.className} antialiased`}>{children}</body>
+      <body className={`${saans.className} antialiased bg-[#EAE5E3]`}>
+        {children}
+      </body>
     </html>
   );
 }
